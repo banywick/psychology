c62ae3bd7308f142696af71019027c2850a35cea
Banywick <android19377@gmail.com>
2023-12-21 09:06:30 +0300

    продолжение

--- work_space.py
+++ work_space.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-from connect_mongo_db import mongo_save, get_one_link
+from connect_mongo_db import mongo_save, get_one_link, connect_mongo_db
 
 """Блок подключения к вебсайту извлечения ссылок на статьи и загрузки в монго"""
 
@@ -13,12 +13,19 @@ for p in range(1):
     link_article = soup.find('div', id='archive-wrapp')  # Получаю ДИВ в ктором лежат 15 статей на одной странице
     for id, link in enumerate(link_article, 1):
         link = 'https://sibac.info/' + link.find('a').get('href')  # Ищу иерархически ссылу и добавляю к ней домен сайта
-        # mongo_save('doc', id, link=link)
-        # print(f'Записано {id}')
+        if connect_mongo_db().find_one({f"doc.{id}": {"$exists": True}}):
+            print('такая запись есть')
+        else:
+            mongo_save('doc', id, link=link)
+            print(f'Записано {id}')
+
+
+
+
 # print(f'Количество ссылок для обработки - {len(data)}')
 
-""" Получаю первую ссылку переходу в нее и запускаю новый парсер на статью"""
-print(get_one_link())
+""" Получаю первую ссылку перехожу в нее и запускаю новый парсер на статью"""
+# print(get_one_link())
 
 
 
